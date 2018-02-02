d2dbccb34a54690617c57f62aee6f13232ae974f
Miroslav Brabenec <brabemi@gmail.com>
2018-02-02 17:31:10 +0100

    Allocation script draft

---
+++ alloc_partitions.py
@@ -0,0 +1,158 @@
+import configparser
+import re
+import time
+
+import click
+import psycopg2
+
+from pprint import pprint
+
+def check_database_cfg(cfg_db):
+    db_params = [
+        'host',
+        'port',
+        'dbname',
+        'username',
+        'password',
+    ]
+    valid = True
+    for param in db_params:
+        if param not in cfg_db:
+            valid = False
+            print('Unable to find {} in database section of config'.format(param))
+    return valid
+
+def create_db_conn(cfg_db):
+    if not check_database_cfg(cfg_db):
+        exit(1)
+    return psycopg2.connect(
+        host=cfg_db['host'],
+        port=cfg_db['port'],
+        dbname=cfg_db['dbname'],
+        user=cfg_db['username'],
+        password=cfg_db['password']
+    )
+
+def check_task_cfg(cfg_task, taskname):
+    params = [
+        'table_name',
+        'period',
+        'count',
+        'part_name',
+        'create_index',
+        'unique_index',
+        'index_postfix',
+        'index_cols',
+    ]
+    valid = True
+    for param in params:
+        if param not in cfg_task:
+            valid = False
+            print('Unable to find {} in {} section of config'.format(param, taskname))
+    return valid
+
+def get_tables(part_name, db):
+    curr = db.cursor()
+    part_name_like = part_name + '%'
+    curr.execute(
+        "SELECT tablename FROM pg_tables where tablename like %s;",
+        (part_name_like,)
+    )
+    return [e[0] for e in curr.fetchall()]
+
+def get_max_no(tables, part_name):
+    prefix_len = len(part_name)
+    max_id = 0
+    for t in tables:
+        part_id = t[prefix_len:]
+        if part_id not in ('lower', 'upper'):
+            part_id = int(part_id)
+            if part_id > max_id:
+                max_id = part_id
+    return max_id
+
+def get_new_tables(min_no, max_no, part_name, period):
+    new_tables = []
+    for no in range(min_no, max_no + 1):
+        new_tables.append({
+            'name': '{}{}'.format(part_name, no),
+            'low': no * period,
+            'up': (no * period) + period
+        })
+    return new_tables
+
+def get_new_indexes(tables, uniq, postfix, cols):
+    indexes = []
+    for t in tables:
+        indexes.append({
+            'name': t['name'] + postfix,
+            'uniq': uniq,
+            'table_name': t['name'],
+            'cols': cols
+        })
+    return indexes
+
+def task(cfg_task, taskname, db):
+    if not check_task_cfg(cfg_task, taskname):
+        print('Unable to procces task {}'.format(taskname))
+        return
+
+    period = int(cfg_task['period'])
+    count = int(cfg_task['count'])
+    part_name = cfg_task['part_name']
+    current_time = int(time.time())
+    part_no = current_time // period
+
+    # TODO check if tables exist lower, upper, one or more ID/NO
+    tables = get_tables(part_name, db)
+    max_no = get_max_no(tables, part_name)
+
+    if max_no > part_no + count:
+        print('Fail max_no: {} > part_no: {} + count: {}'.format(max_no, part_no, count))
+        return
+
+    new_tables = get_new_tables(max_no, part_no + count, part_name, period)
+
+    create_index = cfg_task.getboolean('create_index')
+    new_indexes = []
+    if create_index:
+        unique_index = cfg_task.getboolean('unique_index')
+        index_postfix = cfg_task['index_postfix']
+        index_cols = re.split(r'\W+', cfg_task['index_cols'])
+        new_indexes = get_new_indexes(new_tables, unique_index, index_postfix, index_cols)
+
+    pprint(new_tables)
+    pprint(new_indexes)
+
+    parent_table = cfg_task['table_name']
+
+    # TODO: sql task
+    # unix timestamp / period -> --  1516614109 / 1000000 = 1516
+    # find the newest partition like  part_name + %
+    # select tablename from pg_tables where name like 'history_part_%';
+    # there should be history_part_lower, history_part_upper, history_part_1500
+    #
+
+
+@click.command()
+@click.option(
+    '--config',
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False,
+        writable=False, readable=True, resolve_path=True
+    ),
+    help='path to config file',
+)
+def main(config):
+    cfg = configparser.ConfigParser()
+    cfg.read(config)
+    if 'database' not in cfg:
+        print('Unable to find database section in config')
+        return 1
+    db = create_db_conn(cfg['database'])
+    for section in cfg.sections():
+        if section != 'database':
+            task(cfg[section], section, db)
+
+if __name__ == '__main__':
+    main()
